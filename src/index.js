55a6222c025edae54d201d576a5f41eda21baadf
Rony Antolinez <rony1710.dev@gmail.com>
2022-10-05 21:28:38 -0400

    avocado app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,45 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const URL = "https://platzi-avo.vercel.app/api/avo";
+const root = document.querySelector('#root');
 
-console.log('Happy hacking :)')
+const formatPrice = (price) => {
+    const newPrice = new Intl.NumberFormat('en-En', {
+        style: "currency",
+        currency: "USD",
+    }).format(price)
+
+    return newPrice
+}
+
+const printItems = (items) => {
+    const allItems = []
+    items.map(item => {
+        const image = document.createElement('img')
+        image.src = `https://platzi-avo.vercel.app/${item.image}`
+
+        const title = document.createElement('h2')
+        title.textContent = item.name
+        title.className = 'item-name'
+
+        const price = document.createElement('span')
+        price.textContent = formatPrice(item.price)
+        
+
+        const container = document.createElement('div')
+
+        container.append(image, title, price)
+
+        allItems.push(container)
+        root.append(...allItems)
+    })
+
+}
+
+const fetchData = async (urlApi) => {
+    const res = await fetch(urlApi);
+    const data = await res.json()
+    const allAvocados = data.data
+    printItems(allAvocados)
+}
+
+fetchData(URL);
+
